2c6c3e6cf5543d2e3dd1f0353b70a81dd7dca248
Rishub Nagpal <rishubster@gmail.com>
2017-01-22 22:32:15 -0500

    Update error

--- error.h
+++ error.h
@@ -3,4 +3,21 @@
 
 #include <stdio.h>
 
+enum error_codes
+{
+    E_SUCESS = 0,
+    E_INVALID_INPUT = -1,
+}
+
+typedef error_codes error_t;
+
+struct errordesc {
+    int code;
+    char* message;
+} errordesc = {
+    { E_SUCCESS, "No error" },
+    { E_INVALID_INPUT, "Invalid input" },
+};
+
+
 #endif
