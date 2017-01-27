1d8044a83530f730756c0f789738b1115e024dcc
Rishub Nagpal <rishubster@gmail.com>
2017-01-26 22:57:39 -0500

    bit nonsense

--- fft.c
+++ fft.c
@@ -3,7 +3,16 @@
 #include "error.h"
 #include <limits.h>
 
-bool checkPowerTwo(uint_t n)
+//bit hack magic
+static const int MultiplyDeBruijnBitPosition2[32] = 
+{
+      0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8, 
+        31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
+};
+
+
+
+int checkPowerTwo(uint_t n)
 {
     if(n > UINT_MAX){
         return E_INVALID_INPUT;
@@ -11,28 +20,31 @@ bool checkPowerTwo(uint_t n)
     return ((n & (n -1)) == 0);
 }
 
+//Assumes size is a power of 2
+uint_t generateBitMask(uint_t size)
+{
+    uint_t r = MultiplyDeBruijnBitPosition2[(uint_t)(size * 0x077CB531U) >> 27];
+    return (1 << r) - 1;
+}
 
 uint_t reverseBits(uint_t num)
 {
-    uint_t  num_bits  = sizeof(num) * CHAR_BIT;
-    uint_t reverse_num = 0;
-    int i;
-    for (i = 0; i < num_bits; i++)
-    {
-        if((num & (1 << i)))
-           reverse_num |= 1 << ((num_bits - 1) - i);  
-   }
-    return reverse_num;
+    num = ((num * 0x80200802ULL) & 0x0884422110ULL) * 0x0101010101ULL >> 32;    
+    return num;
 }
 
-dcomp_t* bitReverseCopy(dcomp_t input[], size_t length)
+int bitReverseCopy(dcomp_t input[], dcomp_t output[], size_t length)
 {
-    dcomp_t reversed[length];
+    if(checkPowerTwo(length) == E_INVALID_INPUT || checkPowerTwo(length) == 0)
+    {
+        eprintf(E_INVALID_INPUT, "Given array length is not a power of 2");
+    }
+    uint_t bitMask = generateBitMask(length);
     for(uint_t i = 0; i < length; i++)
     {
-        reversed[reverseBits(i)] = input[i];
+        output[reverseBits(i) & bitMask] = input[i];
     }
-    return reversed;
+    return E_SUCCESS;
 }
 
 
--- fft.h
+++ fft.h
@@ -1,10 +1,16 @@
 #ifndef FFT_H_INCLUDED
 #define FFT_H_INCLUDED
 
-#include <stdbool.h>
-typedef unsigned int uint_t;
+#include <complex.h>
+#include <stddef.h>
 
-bool checkPowerTwo(uint_t n);
+typedef unsigned int uint_t;
+typedef double complex dcomp_t;
+
+int checkPowerTwo(uint_t n);
 uint_t reverseBits(uint_t n);
+uint_t generateBitMask(uint_t size);
+int bitReverseCopy(dcomp_t input[], dcomp_t output[], size_t length);
+
 
 #endif
